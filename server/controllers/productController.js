df3ac3e6d022439a35034ac0d43414f6a8182daa
Sahil Saini <sahilsaini137@gmail.com>
2022-01-06 16:56:37 +0530

    add delete, update, getDetails product routes

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,16 +1,90 @@
 import Product from '../models/productModel.js';
 
-// Create a product
-
+// Create a product :: ADMIN
 export const createProduct = async (req, res) => {
-  const product = await Product.create(req.body);
-  res.status(201).json({
-    success: true,
-    message: 'Product has been successfully created',
-    product,
-  });
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json({
+      success: true,
+      message: 'Product has been successfully created',
+      product,
+    });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
 
+// get all products
 export const getProducts = async (req, res) => {
-  res.status(200).json({ message: 'Route is working' });
+  try {
+    const products = await Product.find();
+
+    res.status(200).json({ success: true, products });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+// Get single product details
+export const getProductDetails = async (req, res) => {
+  try {
+    let id = req.params.id;
+    const product = await Product.findById(id);
+
+    if (!product) {
+      return res.status(500).json({
+        success: true,
+        message: 'Product not found',
+      });
+    }
+    res.status(200).json({ success: true, product });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+// update product :: ADMIN
+export const updateProduct = async (req, res) => {
+  try {
+    let id = req.params.id;
+    let product = Product.findById(id);
+    if (!product) {
+      return res.status(500).json({
+        success: false,
+        message: 'Product not found',
+      });
+    }
+
+    product = await Product.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+
+    res.status(200).json({ success: true, product });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+// Delete product :: ADMIN
+export const deleteProduct = async (req, res) => {
+  try {
+    let id = req.params.id;
+    const product = await Product.findById(id);
+
+    if (!product) {
+      return res.status(500).json({
+        success: true,
+        message: 'Product not found',
+      });
+    }
+
+    await product.remove();
+    res
+      .status(200)
+      .json({ success: true, message: 'Product deleted successfully' });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
--- server/routes/productsRoute.js
+++ server/routes/productsRoute.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import {
   createProduct,
+  deleteProduct,
+  getProductDetails,
   getProducts,
+  updateProduct,
 } from '../controllers/productController.js';
 
 const router = express.Router();
 
 router.get('/', getProducts);
+router.get('/:id', getProductDetails);
 router.post('/create', createProduct);
+router.patch('/:id', updateProduct);
+router.delete('/:id', deleteProduct);
 
 export default router;
