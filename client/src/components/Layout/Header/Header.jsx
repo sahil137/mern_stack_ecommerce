a63fadd9e54c89f427d868a65933507df530afe1
Sahil Saini <sahilsaini137@gmail.com>
2022-04-10 22:45:05 +0530

    fix navbar

--- client/src/components/Layout/Header/Header.jsx
+++ client/src/components/Layout/Header/Header.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { ReactNavbar } from 'overlay-navbar';
+import { FaUserAlt, FaSearch, FaShoppingCart } from 'react-icons/fa';
+import logo from '../../../assets/images/FullLogo.jpeg';
 
 const options = {
-  burgerColorHover: '#eb4034',
-  logoWidth: '20vmax',
   navColor1: 'white',
-  logoHoverSize: '10px',
+  burgerColorHover: '#eb4034',
+  logo,
+  logoWidth: '250px',
   logoHoverColor: '#eb4034',
+  nav2justifyContent: 'space-around',
+  nav3justifyContent: 'space-around',
+  nav1justifyContent: 'flex-end',
+  nav4justifyContent: 'flex-start',
   link1Text: 'Home',
   link2Text: 'Products',
   link3Text: 'Contact',
@@ -15,22 +21,23 @@ const options = {
   link2Url: '/products',
   link3Url: '/contact',
   link4Url: '/about',
-  link1Size: '1.3vmax',
-  link1Color: 'rgba(35, 35, 35,0.8)',
-  nav1justifyContent: 'flex-end',
-  nav2justifyContent: 'flex-end',
-  nav3justifyContent: 'flex-start',
-  nav4justifyContent: 'flex-start',
   link1ColorHover: '#eb4034',
-  link1Margin: '1vmax',
-  profileIconUrl: '/login',
+  link1Color: 'rgba(35, 35, 35,0.8)',
+  link1Size: '1.5rem',
+  link1Padding: '3vmax',
+  profileIcon: true,
+  ProfileIconElement: FaUserAlt,
   profileIconColor: 'rgba(35, 35, 35,0.8)',
-  searchIconColor: 'rgba(35, 35, 35,0.8)',
-  cartIconColor: 'rgba(35, 35, 35,0.8)',
   profileIconColorHover: '#eb4034',
+  searchIcon: true,
+  SearchIconElement: FaSearch,
+  searchIconColor: 'rgba(35, 35, 35,0.8)',
+  cartIcon: true,
+  CartIconElement: FaShoppingCart,
+  cartIconColor: 'rgba(35, 35, 35,0.8)',
   searchIconColorHover: '#eb4034',
   cartIconColorHover: '#eb4034',
-  cartIconMargin: '1vmax',
+  cartIconMargin: '3vmax',
 };
 
 const Header = () => {
